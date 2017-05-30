d1ec21035bfea0bac98c9c21df51e3661a72ddd0
Nahuel Almeira <nahu1990@gmail.com>
2017-05-30 18:40:06 -0300

    Add __init__ and setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='networks',
+      version='0.1',
+      description='Methods for network analysis',
+      url='',
+      author='Nahuel Almeira',
+      author_email='nalmeira@famaf.unc.edu.ar',
+      licence='https://github.com/nahuelalmeira/networks',
+      packages=['networks'],
+      zip_false=False)
